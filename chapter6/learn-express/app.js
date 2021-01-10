33dcaf8058aed40ab4db5b8f4df488c9c2794811
hanjuren <juren528@gmail.com>
2021-01-10 15:14:10 +0900

    미들웨어 사용

--- chapter6/learn-express/app.js
+++ chapter6/learn-express/app.js
@@ -5,8 +5,21 @@ const app = express();
 app.set('port', process.env.PORT || 3000); 
 // app.set은 서버에 포트라는 속성을 3000으로 넣는다.
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html') );
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행됩니다.');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러처리 미들웨어로 갑니다.');
+});
+
+app.use( (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
