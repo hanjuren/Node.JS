218f6418df43840e9908399c39b30647e0e1a342
hanjuren <juren528@gmail.com>
2021-01-22 15:09:05 +0900

    kakao login (passport-kakao)

--- chapter9/nodebird/routes/auth.js
+++ chapter9/nodebird/routes/auth.js
@@ -56,4 +56,20 @@ router.get('/logout', isLoggedIn, (req, res) => {
     res.redirect('/');
 });
 
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+    failureRedirect: '/',
+}), (req, res) => {
+    res.redirect('/')
+});
+
+router.get('/naver', passport.authenticate('naver'));
+
+router.get('/naver/callback', passport.authenticate('naver', {
+    failureRedirect: '/',
+}), (req, res) => {
+    res.redirect('/')
+});
+
 module.exports = router;
