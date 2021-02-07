d0ae4cf710e7b947fcc45f28c2b8bb349c38b535
hanjuren <juren528@gmail.com>
2021-02-07 18:30:27 +0900

    미들웨어와 소켓 연결하기

--- chapter12/gif-chat/app.js
+++ chapter12/gif-chat/app.js
@@ -21,12 +21,7 @@ nunjucks.configure('views', {
 });
 connect();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -34,10 +29,16 @@ app.use(session({
         httpOnly: true,
         secure: false,
     },
-}));
+});
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
 
 app.use((req, res, next) => {
-    if(!req.session.color){
+    if (!req.session.color) {
         const colorHash = new ColorHash();
         req.session.color = colorHash.hex(req.sessionID);
     }
@@ -47,7 +48,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
 });
@@ -59,9 +60,9 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-const server = app.listen(app.get('port'), () =>{
-    console.log(app.get('port'), '번 포트에서 대기 중');
+const server = app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app); // app을 넘기는 이유는 socket.js 에서 app.set('io') 하기 위해서 넘기는것
+webSocket(server, app, sessionMiddleware); // app을 넘기는 이유는 socket.js 에서 app.set('io') 하기 위해서 넘기는것
 
--- chapter12/gif-chat/socket.js
+++ chapter12/gif-chat/socket.js
@@ -1,69 +1,68 @@
-// socket io 사용
-const SoketIO = require('socket.io');
+const SocketIO = require('socket.io');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie-signature');
 
-module.exports = (server, app) => {
-    const io = SoketIO(server, {path: '/socket.io' });
-    app.set('io', io); // 라우터에서 req.get('io)로 접근 가능
+module.exports = (server, app, sessionMiddleware) => {
+    const io = SocketIO(server, { path: '/socket.io' });
+    app.set('io', io);
     const room = io.of('/room');
     const chat = io.of('/chat');
+    // soket.io 2버전
+    /*io.use((socket, next) => {
+        cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res || {}, next);
+        sessionMiddleware(socket.request, socket.request.res || {}, next);
+    }); */
+    // soket.io 3버전
+    const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+    chat.use(wrap(cookieParser(process.env.COOKIE_SECRET)));
+    chat.use(wrap(sessionMiddleware));
 
-
-room.on('connection', (socket) => { 
-    console.log('room  네임스페이스 접속');
-    socket.on('disconnect', () => {
+    room.on('connection', (socket) => {
+        console.log('room 네임스페이스에 접속');
+        socket.on('disconnect', () => {
         console.log('room 네임스페이스 접속 해제');
+        });
     });
-});
 
-chat.on('connection', (socket) => {
-    console.log('chat 네임스페이스 접속');
-    const req = socket.request;
-    const { headers: { referer } } = req;
-    // url 에서 roomid를 추출 하는 부분
-    const roomId = referer
-        .split('/')[referer.split('/').length -1]
-        .replace(/\?.+/,'');
-    // 밑의 join 과 leave는 웹 소켓에서 제공하는 메서드
-    socket.join(roomId);
+    chat.on('connection', (socket) => {
+        console.log('chat 네임스페이스에 접속');
+        const req = socket.request;
+        const { headers: { referer } } = req;
+        const roomId = referer
+        .split('/')[referer.split('/').length - 1]
+        .replace(/\?.+/, '');
+        socket.join(roomId);
+        socket.to(roomId).emit('join', {
+        user: 'system',
+        chat: `${req.session.color}님이 입장하셨습니다.`,
+    });
 
     socket.on('disconnect', () => {
         console.log('chat 네임스페이스 접속 해제');
         socket.leave(roomId);
-    });
-});
-
-};
-
-
-
-
-
-
-/* 웹 소켓 사용
-const WebSoket = require('ws'); 
-
-module.exports = (server) => {
-
-    const wss = new WebSoket.Server({ server }); // 서버에 이벤트 리스너 생성
-
-    wss.on('connection', (ws, req) => { //웹 소켓 연결 시 (클라이언트에서 연결) connection은 서버와 웹 소켓이 연결을 맺을때 발생
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; // 클라이언트의 IP를 알아내는 방범
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => { // 클라이언트로 부터 메시지 수신 시 send로 보내면 수신
-            console.log(message);
-        });
-        ws.on('error', (error) => { //에러 시
-            console.error(error);
-        });
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
-        });
-
-        ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-            if(ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+        const currentRoom = socket.adapter.rooms[roomId];
+        const userCount = currentRoom ? currentRoom.length : 0;
+        if (userCount === 0) { // 유저가 0명이면 방 삭제
+            const signedCookie = cookie.sign( req.signedCookies['connect.sid'], process.env.COOKIE_SECRET );
+            const connectSID = `${signedCookie}`;
+            axios.delete(`http://localhost:8005/room/${roomId}`, {
+            headers: {
+                Cookie: `connect.sid=s%3A${connectSID}`
             }
-        }, 3000);
+        })
+          .then(() => {
+            console.log('방 제거 요청 성공');
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        } else {
+            socket.to(roomId).emit('exit', {
+                user: 'system',
+                chat: `${req.session.color}님이 퇴장하셨습니다.`,
+            });
+        }
     });
-}; */
+  });
+};
